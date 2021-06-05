47b08d99e2c4251410cb405dc006af2595f06916
cekvenich2 <76924532+cekvenich2@users.noreply.github.com>
2021-06-05 14:10:31 -0500

    sync

--- docs/starter/public/mapp/my-comp.js
+++ docs/starter/public/mapp/my-comp.js
@@ -1,22 +1,46 @@
-// based on https://developers.google.com/web/fundamentals/web-components/customelements
+// based on http://developers.google.com/web/fundamentals/web-components/customelements
+// and http://docs.ficusjs.org
+
 export class MyComp extends HTMLElement {
 
-	template = `<b>I'm a Cust. El</b>
-	<slot></slot>`;
+	template = `
+	<style>
+		:host{
+			color: red;
+		}
+	</style
+	<b>I'm a Cust. El</b>
+	<slot></slot>
+	<p id='p123'>X</p>
+	`;
+
+	sr // shadow root handle
 
 	constructor() {
 		super()
-		this._setup(this.template) // just a helper function for boiler plate.
+		this.setup(this.template) // just a helper function for boiler plate
+		this.getData()
+	}
+
+	getData= () => { // example of course
+		fetch('https://jsonplaceholder.typicode.com/todos/1')
+  			.then(this.render('World'))
+	}
+
+	render= (arg) => {
+		let n = this.query('#p123')
+		console.log(n)
+		n.innerHTML=`Hello ${arg}` // es string template
 	}
 
 	// bolierplate: /////////////////
-	sr // shadow root handle
+	el // element handle
 
    /**
 	* A helper method
 	* @param template 
 	*/
-	_setup(template) {
+	setup(template) {
 		const cTemp = document.createElement('template')
 		cTemp.innerHTML = template
   
@@ -24,18 +48,11 @@ export class MyComp extends HTMLElement {
 		this.sr.appendChild(cTemp.content.cloneNode(true))
 	 }//cons
 
-	//- eg addScript('bla.js', null, 'api-key', 'key123') when they want you to use the tag: so you can in your own sequence
-	_addScript(src, callback, attr, attrValue, id) {
-		var s = document.createElement('script')
-		s.setAttribute('src', src)
-		if (attr) s.setAttribute(attr, attrValue)
-		if (id) s.id = id
-		if (callback) s.onload = callback
-		s.async = false 
-		document.getElementsByTagName('head')[0].appendChild(s)
-	};
+	query(sel) {
+		return this.sr.querySelector(sel)
+	}
 
-	_getSlotElById(id) {
+	getSlotElById(id) {
 		let ret
 		this.slotEls.map(function(n){
 		   if(n.id==id) ret = n
@@ -46,11 +63,21 @@ export class MyComp extends HTMLElement {
 	 /**
 	  * Get elements in a slot
 	  */
-	_getSlotEls() {
+	getSlotEls() {
 		// https://javascript.info/slots-composition
 		return this.sr.querySelector('slot').assignedElements()
 	}   
-  
-}
+
+	//- eg addScript('bla.js', null, 'api-key', 'key123') when they want you to use the tag: so you can in your own sequence
+	addScript(src, callback, attr, attrValue, id) {
+		var s = document.createElement('script')
+		s.setAttribute('src', src)
+		if (attr) s.setAttribute(attr, attrValue)
+		if (id) s.id = id
+		if (callback) s.onload = callback
+		s.async = false 
+		document.getElementsByTagName('head')[0].appendChild(s)
+	}//()
+}//class
 customElements.define('my-comp', MyComp)
 console.log('loaded')
