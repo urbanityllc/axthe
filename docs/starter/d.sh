c4c3d982e916d8cfc76f1a3430c2249aeef1d31c
cekvenich2 <76924532+cekvenich2@users.noreply.github.com>
2021-06-09 18:59:32 -0500

    bs 501

--- docs/starter/d.sh
+++ docs/starter/d.sh
@@ -2,8 +2,10 @@
 clear
 npm i -g npm
 npm i -g npm-check-updates
+npm i -g npm-run-all
 ncu
 npm i
 
-# uses nodemon to monitor js in lib
-npm start
+# uses nodemon to monitor js in lib and startCss to monitorSass
+npm-run-all --parallel watchSass watchNode
+
