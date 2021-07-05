3d9f31fe7c51e7caffd9eee31fe9da38360aced9
cekvenich2 <>
2021-07-05 10:45:06 -0500

    11

--- delMe/11/.eleventy.js
+++ delMe/11/.eleventy.js
@@ -1,11 +1,40 @@
 // 11ty configuration
-module.exports = config => {
+// http://snipcart.com/blog/11ty-tutorial
+
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+const pluginSass = require("eleventy-plugin-sass")
+const pug = require("pug")
+
+module.exports = eleventyConfig => {
+	
+	eleventyConfig.setLibrary("pug", pug)
+	eleventyConfig.setPugOptions({  })
+
+	eleventyConfig.addPlugin(eleventyNavigationPlugin)
+	eleventyConfig.addPlugin(pluginSass, {})
+
+
+	eleventyConfig.addPassthroughCopy("css")
+	eleventyConfig.addPassthroughCopy("js")
+	eleventyConfig.addPassthroughCopy("images")
+	eleventyConfig.addPassthroughCopy("fonts")
+	eleventyConfig.addPassthroughCopy("./favicon.ico")
+	eleventyConfig.addPassthroughCopy("./robots.txt")
+	eleventyConfig.addPassthroughCopy("./ads.txt")
+	eleventyConfig.addPassthroughCopy("./_redirects")
+	eleventyConfig.addPassthroughCopy("./*.png")
+
 	return {
-  
-	  dir: {
+		"dataTemplateEngine": "pug",
+		markdownTemplateEngine: "pug",
+		htmlTemplateEngine: "pug",
+		templateFormats: ["pug"],
+
+		pathPrefix: '/site2',
+
+		dir: {
 		input: 'src',
-		output: 'www'
-	  }
-  
-	};
-  };
+		output: 'static'
+		}
+	}
+};
--- delMe/11/d.sh
+++ delMe/11/d.sh
@@ -1,1 +1,6 @@
+ckear
+rm -rf _site
+
 npx @11ty\/eleventy --serve
+
+# npx @11ty/eleventy build
