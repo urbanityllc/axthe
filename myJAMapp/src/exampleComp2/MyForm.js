d799bbe8b55d894f1321286be048e139a88ca2bc
cekvenich2 <>
2021-07-05 20:33:23 -0500

    form

--- myJAMapp/src/exampleComp2/MyForm.js
+++ myJAMapp/src/exampleComp2/MyForm.js
@@ -1,52 +1,43 @@
 
-
 import { createCustomElement } from 'ficusjs/custom-element'
 import { html, renderer } from '@ficusjs/renderers/htm'
 
+// https://github.com/axthe/committers/blob/8d3f7c9ee39de7d804de3b086bfbfb4305c24c16/axtheLib/CDN/js/AXslotHe.js
 
 createCustomElement('my-form', {
 	mounted () {
 		console.log('oh hi')
-		const el = document.getElementById('but1')
-		console.log(el)
+		const but1 = document.getElementById('but1')
+		but1.addEventListener("click", (e) => {
+			e.preventDefault()
+			console.log(e)
+
+			const data = {}
+			const fdata = new FormData(document.getElementById('form1'))
+			for (var [key, value] of fdata.entries()) {
+				data[key] =value;
+			}
+
+			fetch("/wapi/email",
+				{ method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(data)
+				}).then(res => {
+					console.log('status ', res.statusText)
+				})
+		})
+
 	},
 	renderer,
 	handleClick () { },
 
 	render () {
 		return html`
-		<form>
+		<form id='form1'>
 			${this.slots.default}
 		</form>
 	`
 	}
 })
-
-// https://github.com/axthe/committers/blob/8d3f7c9ee39de7d804de3b086bfbfb4305c24c16/axtheLib/CDN/js/AXslotHe.js
-/*
-function onMount() {
-
-	$('#mailFrm').on('submit', function(e) {
-		e.preventDefault()
-
-			const data = {}
-			const fdata = new FormData(document.getElementById('mailFrm'))
-			for (var [key, value] of fdata.entries()) {
-				data[key] =value;
-			}
-			console.log(JSON.stringify(data))
-
-			fetch("/wapi/email",{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify(data),
-				})
-				.then(res => {
-					console.log('emailing ', res.statusText)
-				})
-
-	})
-}
-*/
