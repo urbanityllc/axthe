3c648b46b768fabdf6ea2315a9bebc8e7b194c79
cekvenich2 <>
2021-07-07 11:53:35 -0500

    form

--- myJAMapp/src/exampleComp2/MyForm.js
+++ myJAMapp/src/exampleComp2/MyForm.js
@@ -5,39 +5,28 @@ import { html, renderer } from '@ficusjs/renderers/htm'
 // https://github.com/axthe/committers/blob/8d3f7c9ee39de7d804de3b086bfbfb4305c24c16/axtheLib/CDN/js/AXslotHe.js
 
 createCustomElement('my-form', {
-	mounted () {
-		console.log('oh hi')
-		const but1 = document.getElementById('but1')
-		but1.addEventListener("click", (e) => {
-			e.preventDefault()
-			console.log(e)
-
-			const data = {}
-			const fdata = new FormData(document.getElementById('form1'))
-			for (var [key, value] of fdata.entries()) {
-				data[key] =value;
-			}
-
-			fetch("/wapi/email",
-				{ method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify(data)
-				}).then(res => {
-					console.log('status ', res.statusText)
-				})
-		})
-
-	},
-	renderer,
-	handleClick () { },
-
 	render () {
 		return html`
-		<form id='form1'>
+		<form onsubmit="${this.onSubmit}">
+		<fieldset>
 			${this.slots.default}
+		</fieldset>
 		</form>
 	`
-	}
+	},
+	onSubmit (e) {
+		e.preventDefault()
+		const data = Object.fromEntries(new FormData(e.target).entries())
+		console.log(data)
+		fetch("/wapi/email",
+			{ method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			}).then(res => {
+				console.log('status ', res.statusText)
+			})
+	},
+	renderer
 })
