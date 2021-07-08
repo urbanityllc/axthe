d208e09f041a6fcaf3e1732d316b6bee5eccc447
cekvenich2 <>
2021-07-08 13:31:35 -0500

    supa

--- myAPIapp/testi/test1.test.js
+++ myAPIapp/testi/test1.test.js
@@ -1,4 +1,21 @@
 'use strict';
+const debug = require('debug')
+if (debug.enabled) console.log('debug enabled')
+const fs = require('fs-extra')
+try {
+	fs.readFileSync('.env', 'utf8')
+	let dotEnv = require('dotenv').config().parsed
+	console.log('.env keys:')
+	for (const k in dotEnv) {
+		console.log(k)
+	  }
+	console.log()
+  } catch (err) {
+	console.error(err)
+	console.error(`.env file does not exist, this won't work`)
+	process.exit(1)
+}
+// ***
 
 const test = require('tape')
 const AnBModel     = require('../lib/AnBModel.js')
