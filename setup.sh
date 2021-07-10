1bc1df3b4856e91d8aecddb2200d5b9333ffd9fd
cekvenich2 <76924532+cekvenich2@users.noreply.github.com>
2021-07-10 16:23:49 -0500

    docs

--- setup.sh
+++ setup.sh
@@ -3,16 +3,22 @@
 
 apt-get update
 apt-get upgrade -y
-#reboot
+apt-get dist-upgrade -y
+# reboot
 
+apt-get install python3.9 -y
+apt-get install python3-pip -y
+alias python='python3'
+apt-get install build-essential -y
 apt-get install unzip -y
 apt-get install git -y
-apt-get install openssh-client
+apt-get install openssh-client -y
 
 # node app server via nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
-nvm install 14.17.2 -y
+nvm install 14.17.3 -y
+nvm alias default node
 npm i -g npm
 npm install -g cordova
 # install a tester for node:
@@ -20,6 +26,7 @@ npm i -g colortape
 npm i -g tape
 npm install pm2@latest -g
 npm i -g yarn
+npm -v
 
 # caddy http server
 # https://caddyserver.com/docs/install#debian-ubuntu-raspbian
@@ -34,7 +41,14 @@ apt install cockpit -y
 # above is a monitoring server, read https://cockpit-project.org/documentation.html
 # for now it is ok to accept a bad cert, you can fix it w/ Caddy later via proxy to port 9090 to monitor your OS
 
+# just a clean up step
+apt autoremove -y
+
 # get AXthe starter, and unzip
-wget https://axthe.github.io/axthe/starter.zip
-unzip starter.zip
-mv starter myApp
+wget https://github.com/axthe/axthe/archive/refs/heads/main.zip
+unzip main.zip
+rm *.zip
+
+# optional, if you install PHP, install latest v7, not v8
+apt -y install php7.4
+php -v
